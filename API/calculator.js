e7058f6d7d4516a5284878706f8446e9fd3fb82e
Oshin Lilligirl <oshingp@Oshins-MacBook-Pro.local>
2024-12-20 20:34:16 +0700

    install express

--- API/calculator.js
+++ API/calculator.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
