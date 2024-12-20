2221c8e3c649242e9a887e7bd4982ae0f055f0a1
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2024-12-20 17:30:43 +0700

    Update function

--- easy-quiz/script.js
+++ easy-quiz/script.js
@@ -1,10 +1,43 @@
+const result = document.getElementById("result");
+const result2 = document.getElementById("result2");
+const quizzes = document.getElementById("quizzes");
+const score = document.createElement("div");
+
+let count = 0;
+
+function updateScore() {
+  score.textContent = `Your scores = ${count}`;
+}
+
+updateScore();
+quizzes.appendChild(score);
+
 function checkAnswer(answer) {
-   const result = document.getElementById('result');
-   if (answer === 'a') {
-       result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
-       result.style.color = 'green';
-   } else {
-       result.textContent = "Incorrect! Try again.";
-       result.style.color = 'red';
-   }
+  if (answer === "a") {
+    result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
+    result.style.color = "green";
+    count++;
+  } else {
+    result.textContent = "Incorrect! Try again.";
+    result.style.color = "red";
+    if (count > 0) {
+      count--;
+    }
+  }
+  updateScore();
+}
+
+function checkAnswer2(answer) {
+  if (answer === "c") {
+    result2.textContent = "Great Job! I like white colour";
+    result2.style.color = "green";
+    count++;
+  } else {
+    result.textContent = "Incorrect! Try again.";
+    result.style.color = "red";
+    if (count > 0) {
+      count--;
+    }
+  }
+  updateScore();
 }
